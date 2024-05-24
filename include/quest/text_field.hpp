e7c2f310bb946ee23a03861365a114781a1c0572
lyorig <petrik.sacha@gmail.com>
2024-05-24 11:45:43 +0200

    twe

--- include/quest/text_field.hpp
+++ include/quest/text_field.hpp
@@ -15,12 +15,12 @@ namespace HQ {
         text_field();
 
         // Process some text. Returns the amount of chars that were added.
-        std::size_t process(std::string_view inp);
+        void process(std::string_view inp);
 
         // Process a key. Returns the amount of characters added/removed.
         diff_t process(hal::keyboard::key k, hal::keyboard::mod_state m, const hal::proxy::clipboard& c);
 
         std::string text;
-        hal::u16    cursor;
+        std::size_t cursor;
     };
 }
