9c369ee9cd078440d49bc48d27e0143a9c833d95
Petr Šácha <petrik.sacha@gmail.com>
2026-03-14 07:41:07 +0100

    ⚡️ data_loader: delegate to resource_loader

--- include/quest/data_loader.hpp
+++ include/quest/data_loader.hpp
@@ -5,11 +5,13 @@
 namespace hq {
     class data_loader {
     public:
-        data_loader() = default;
+        data_loader();
 
         std::string resolve(std::string_view path) const;
 
     private:
+        std::string m_base;
+
         hal::fs::resource_loader m_loader;
     };
 }
--- src/data_loader.cpp
+++ src/data_loader.cpp
@@ -1,9 +1,21 @@
 #include <quest/data_loader.hpp>
 
-#include <format>
-
 using namespace hq;
 
+namespace {
+    std::string data_path() {
+        std::string s { hal::fs::base_path() };
+        s.resize(s.find_last_of('/', s.find_last_of('/') - 1) + 1);
+        s += "data/";
+        return s;
+    }
+}
+
+data_loader::data_loader()
+    : m_base { data_path() }
+    , m_loader { m_base } {
+}
+
 std::string data_loader::resolve(std::string_view path) const {
-    return m_loader.resolve(std::format("../data/{}", path));
+    return m_loader.resolve(path);
 }
